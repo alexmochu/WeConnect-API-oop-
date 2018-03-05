c248c952c7bf0c2c672df5508c15e465a1d9398b
Mochualex4 <Mochualex4@gmail.com>
2018-03-05 06:42:29 +0300

    Create welcome message of the API

---
+++ api/views.py
@@ -0,0 +1,15 @@
+# api/__init__.py
+
+from flask import jsonify
+from flask_api import FlaskAPI
+
+ # Initialize the api
+app = FlaskAPI(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+ 
+# Home route
+@app.route('/')
+def hello_world():
+    response = jsonify({'greetings': 'Greetings and welcome to weConnect API'})
+    return response

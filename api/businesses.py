d1ef461fe2992b09b4644df2e46741d25f59e9ab
Mochualex4 <Mochualex4@gmail.com>
2018-03-08 06:49:32 +0300

    Create BusinessesClass which contains methods that allow user to create and view Businesses and its API route - [#155694990]

---
+++ api/businesses.py
@@ -0,0 +1,41 @@
+# api/businesses
+
+class BusinessesClass(object):
+    """Handles creation of events"""
+
+    def __init__(self):
+        """ list to hold events a user creates """
+        self.businesses_list = []
+
+    def get_all_businesses(self):
+        """Gets and Displays all Business items created and stored in the business lists"""
+        return self.businesses_list
+
+    def create_business(self, business_name, user, category, location):
+        """Handles creation of events"""
+        business_dict = {}
+
+        #Check for special characters
+        for business in self.businesses_list:
+            if business_name == business["business_name"]:
+                return "Please enter a Unique business name, business name already taken"
+
+        if len(business_name) < 6:
+            return "Input a business name that is atleast 6 characters"
+
+        elif len(category) < 6:
+            return "Your caegory name should be atleast 6 characters"
+
+        elif len(location) < 5:
+            return "Your location name should be atleast 5 characters"
+
+        elif len(user) > 6:
+            business_dict['business_name'] = business_name
+            business_dict['owner'] = user
+            business_dict['category'] = category
+            business_dict['location'] = location
+
+            self.businesses_list.append(business_dict)
+        else:
+            return "User name length is less than 8 characters"
+        return "Business added successfully. Add another business page"
--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from flask_api import FlaskAPI
 
 # local imports
 from api.user_accounts import UserClass
+from api.businesses import BusinessesClass
 
 # Initialize the api
 app = FlaskAPI(__name__, instance_relative_config=True)
@@ -12,6 +13,7 @@ app.config.from_pyfile('config.py')
 
 
 user_object = UserClass()
+business_object = BusinessesClass()
 
 # Home route
 @app.route('/')
@@ -20,6 +22,7 @@ def home_route():
     response = jsonify({'greetings': 'Greetings and welcome to weConnect API'})
     return response
 
+# Registration Route
 @app.route('/api/v1/auth/register', methods=['GET', 'POST'])
 def signup():
     """ User register """
@@ -33,18 +36,41 @@ def signup():
         response.status_code = 201
         return response
 
+# Login Route
 @app.route('/api/v1/auth/login', methods=['GET', 'POST'])
 def login():
     """ User login """
     if request.method == "POST":
         username = request.json['username']
         password = request.json['password']
-        session['username'] = username            
+        session['username'] = username
         msg = user_object.login(username, password)
         response = jsonify(msg)
         response.status_code = 200
         return response
 
+# Create and view businesses
+@app.route('/api/v1/business', methods=['GET', 'POST'])
+def create_business():
+    """ create event """
+    if session.get('username') is not None:
+        if request.method == "POST":
+            business_name = request.json['business_name']
+            user = request.json['user']
+            location = request.json['location']
+            category = request.json['category']
+
+            msg = business_object.create_business(business_name, user, category, location)
+            response = jsonify(msg)
+            response.status_code = 201
+            return response
+        elif request.method == "GET":
+            msg = business_object.get_all_businesses()
+            response = jsonify(msg)
+            return response
+    return jsonify({"message": "Please Login"})
+
+# Logout and remove session
 @app.route('/api/v1/auth/logout', methods=['POST'])
 def logout():
     """ Logging out """

059dd9832f3733958961e4fe480d83c0faccfdec
Mochualex4 <Mochualex4@gmail.com>
2018-04-16 16:51:21 +0300

    add unit test cases for checking business review items [#156802757]

--- tests/test_businesses.py
+++ tests/test_businesses.py
@@ -7,13 +7,10 @@ class TestBusinessItemsTestCases(unittest.TestCase):
 
     def setUp(self):
         """ Setup Business Class Class test case """
-
         self.business_item_class = BusinessesClass()
-
 
     def test_special_characters_business_name(self):
         """ Check for special characters while creating a business item """
-        
         user = "chairman"
         msg = self.business_item_class.create_business("Ma@en_deleo", user, "soft*%ware", "nairo@&")
         print(msg)
@@ -21,8 +18,7 @@ class TestBusinessItemsTestCases(unittest.TestCase):
 
     def test_get_all_businesses(self):
         """ Check for all businesses """
-        self.business_item_class.businesses_list = [{"owner": "chairman", "business_name":"Maendeleo", "category":"Backaend", "location":"myhomecity"},
-                                                 {"owner": "chairmanwe", "business_name":"NshMaendeleo", "category":"Backaend", "location":"myhomecity"}]
+        self.business_item_class.businesses_list = [{"owner": "chairman", "business_name":"Maendeleo", "category":"Backaend", "location":"myhomecity"}, {"owner": "chairmanwe", "business_name":"NshMaendeleo", "category":"Backaend", "location":"myhomecity"}]
         msg = self.business_item_class.businesses_list
 
         value = self.business_item_class.get_all_businesses()
@@ -34,7 +30,7 @@ class TestBusinessItemsTestCases(unittest.TestCase):
                                                  {"owner": "chairmanwe", "business_name":"NshMaendeleo", "category":"Backaend", "location":"myhomecity"}]
         user = "chairman"
         msg = self.business_item_class.getOwner(user)
-        self.assertEqual(msg, [{"owner": "chairman", "business_name":"Maendeleo", "category":"Backaend", "location":"myhomecity"}]) 
+        self.assertEqual(msg, [{"owner": "chairman", "business_name":"Maendeleo", "category":"Backaend", "location":"myhomecity"}])
 
     def test_correct_output(self):
         """ Check for correct business creation """
@@ -44,12 +40,10 @@ class TestBusinessItemsTestCases(unittest.TestCase):
 
     def test_update_business(self):
         """ Check for edits to business name """
-        
         self.business_item_class.businesses_list = [{"owner": "chairman", "business_name":"Maendeleo", "category":"Backaend", "location":"myhomecity"},
                                                  {"owner": "chairmanwe", "business_name":"NshMaendeleo", "category":"Backaend", "location":"myhomecity"}]
         msg = self.business_item_class.update_business("Christmass", "Maendeleo", "chairman")
         self.assertEqual(msg, [{"owner": "chairman", "business_name":"Christmass", "category":"Backaend", "location":"myhomecity"}])
-        
 
     def tearDown(self):
         """ Teardown Business Class test case  """
